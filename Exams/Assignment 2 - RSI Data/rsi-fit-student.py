c8e39c4452b11bb4fd554d41347bb5acea865dfe
faymj_ <faymj@dukes.jmu.edu>
2026-04-23 19:52:42 -0400

    some updates?
    title

--- Exams/Assignment 2 - RSI Data/rsi-fit-student.py
+++ Exams/Assignment 2 - RSI Data/rsi-fit-student.py
@@ -6,19 +6,59 @@ import matplotlib.pyplot as plt
 """
 Preamble: Load data from source CSV file
 """
-path_to_datafile = "../../data/drop-jump/all_participant_data_rsi.csv"
+path_to_datafile = "././data/drop-jump/all_participant_data_rsi.csv"
 
-### YOUR CODE HERE
+#Use examples from the last assignment to check that we are loading the file right
+try:
+    rawdataset = open(path_to_datafile)
+except FileNotFoundError:
+    print(path_to_datafile + " not found. please amend file path as neccessary (line 9)")
+
+#setting up variables
+entry = []
+dataset = []
+done = 0
+trial = 0
+force_plate_rsi = 0
+accelerometer_rsi = 0
+percent_error = 0
+
+
+#Proccess the file information to something more useful (inspired by the mechanisms behind `Assignment 1`)
+while not done:
+    #turn out it is essential this comes first, or else the thing gets an unpack error.
+    line = rawdataset.readline()
+    if line == '':
+        done = True
+        continue
+    
+    #unpacking action (I find it weird that this doesn't need int conversion, but maybe that cause its already ints?)
+    (trial, force_plate_rsi, accelerometer_rsi, percent_error) = line.rstrip().split(",")
+    entry = (trial, force_plate_rsi, accelerometer_rsi, percent_error)
+    
+    #add to our dataset for the computer to read
+    dataset.append(entry)
+done = 1
+
+print(dataset)
 
 """
 Question 1: Load the force plate and acceleration based RSI data for all participants. Map each data set (accel and FP)
-to a normal distribution. Clearly report the distribution parameters (mu and std) and generate a graph two each curve's 
+to a normal distribution. Clearly report the distribution parameters (mu and std) and generate a graph to each curve's 
 probability distribution function. Include appropriate labels, titles, and legends.
 """
 print('-----Question 1-----')
+ 
+#creating a plot by mapping each set of letters onto a specific temp variable, and rendering that
 
-### YOUR CODE HERE
-
+#UNFINISHED
+# create x-axis for chi2 plot
+plt.plot(force_plate_rsi, accelerometer_rsi, label='test label')
+plt.title('Data mapped')
+plt.xlabel('Force plate (N)')
+plt.ylabel('Acceleration')
+plt.legend()
+plt.show()
 
 """
 Question 2: Conduct a Chi2 Goodness of Fit Test for each dataset to test whether the data is a good fit
